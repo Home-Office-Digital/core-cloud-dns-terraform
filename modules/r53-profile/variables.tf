5751ed6ed8db3373ae396880a30d87d6b42ecf78
Vijay Jadhav <mail2vjadhav@gmail.com>
2026-06-04 15:27:52 +0100

    fix: Separate EBSA Prod and NotProd

--- modules/r53-profile/variables.tf
+++ modules/r53-profile/variables.tf
@@ -32,14 +32,24 @@ variable "poise_dns_ips" {
   # default = ["10.228.8.69", "10.228.9.69"]
 }
 
-variable "ebsa_domain_names" {
+variable "ebsa_notprod_domain_names" {
   type = list(string)
 }
 
-variable "ebsa_dns_ips" {
+variable "ebsa_prod_domain_names" {
+  type = list(string)
+}
+
+variable "ebsa_notprod_dns_ips" {
   type = list(string)
   # example:
-  # default = ["10.228.8.69", "10.228.9.69"]
+  # default = ["10.223.6.134", "10.223.6.6"]
+}
+
+variable "ebsa_prod_dns_ips" {
+  type = list(string)
+  # example:
+  # default = ["10.223.2.134", "10.223.2.6"]
 }
 
 variable "ncsc_dns_ips" {
