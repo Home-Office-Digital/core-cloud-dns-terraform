21e5bc7dfd6cc121b4af216733b6194d894b6027
Vijay Jadhav <mail2vjadhav@gmail.com>
2026-07-08 11:44:38 +0100

    ops: Adding APA HOB IBM DNS servers

--- modules/r53-profile/r53-outbound-resolver-rules.tf
+++ modules/r53-profile/r53-outbound-resolver-rules.tf
@@ -152,3 +152,79 @@ resource "aws_route53profiles_resource_association" "ebsa_prod_assoc" {
   profile_id   = aws_route53profiles_profile.cc_r53_profile.id
   resource_arn = each.value.arn
 }
+
+#############################
+# Resolver rules - APA HOB IBM PREPROD (one per domain)
+#############################
+resource "aws_route53_resolver_rule" "apa_hob_ibm_preprod" {
+  for_each = toset(var.apa_hob_ibm_preprod_domain_names)
+
+  name                 = substr("fwd-to-apa-hob-ibm-preprod-${replace(each.key, ".", "-")}", 0, 64)
+  domain_name          = each.key
+  rule_type            = "FORWARD"
+  resolver_endpoint_id = aws_route53_resolver_endpoint.cc_poise_outbound_endpoint.id
+
+  dynamic "target_ip" {
+    for_each = var.apa_hob_ibm_preprod_dns_ips
+    content {
+      ip = target_ip.value
+    }
+  }
+
+  tags = merge(
+    var.tags,
+    {
+      RuleType    = "FORWARD"
+      Description = "Forwards ${each.key} to APA HOB IBM PREPROD DNS"
+    }
+  )
+}
+
+#############################
+# Associate each APA HOB IBM PREPROD rule to the R53 Profile
+#############################
+resource "aws_route53profiles_resource_association" "apa_hob_ibm_preprod_assoc" {
+  for_each = aws_route53_resolver_rule.apa_hob_ibm_preprod
+
+  name         = substr("cc-apa-hob-ibm-preprod-assoc-${substr(md5(each.key), 0, 8)}", 0, 64)
+  profile_id   = aws_route53profiles_profile.cc_r53_profile.id
+  resource_arn = each.value.arn
+}
+
+#############################
+# Resolver rules - APA HOB IBM PROD (one per domain)
+#############################
+resource "aws_route53_resolver_rule" "apa_hob_ibm_prod" {
+  for_each = toset(var.apa_hob_ibm_prod_domain_names)
+
+  name                 = substr("fwd-to-apa-hob-ibm-prod-${replace(each.key, ".", "-")}", 0, 64)
+  domain_name          = each.key
+  rule_type            = "FORWARD"
+  resolver_endpoint_id = aws_route53_resolver_endpoint.cc_poise_outbound_endpoint.id
+
+  dynamic "target_ip" {
+    for_each = var.apa_hob_ibm_prod_dns_ips
+    content {
+      ip = target_ip.value
+    }
+  }
+
+  tags = merge(
+    var.tags,
+    {
+      RuleType    = "FORWARD"
+      Description = "Forwards ${each.key} to APA HOB IBM PROD DNS"
+    }
+  )
+}
+
+#############################
+# Associate each APA HOB IBM PROD rule to the R53 Profile
+#############################
+resource "aws_route53profiles_resource_association" "apa_hob_ibm_prod_assoc" {
+  for_each = aws_route53_resolver_rule.apa_hob_ibm_prod
+
+  name         = substr("cc-apa-hob-ibm-prod-assoc-${substr(md5(each.key), 0, 8)}", 0, 64)
+  profile_id   = aws_route53profiles_profile.cc_r53_profile.id
+  resource_arn = each.value.arn
+}
